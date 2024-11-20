93b742b27b6dc694f6b4acb028a7f8dc418df7bc
Wilbert Castillo <106358166+Wilbert-Miau@users.noreply.github.com>
2024-11-20 16:16:23 -0600

    feat: get client ip address

---
+++ src/app/services/client-info.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientInfoService {
+
+  private apiUrl = 'https://api.ipify.org/?format=json';
+
+  constructor(private http: HttpClient) { }
+
+  getIPAddress(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
