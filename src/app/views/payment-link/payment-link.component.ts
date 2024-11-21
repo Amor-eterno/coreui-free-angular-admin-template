879503e0286cf68c03c004caad22df794cb15fc8
Wilbert Castillo <106358166+Wilbert-Miau@users.noreply.github.com>
2024-11-20 18:01:50 -0600

    feat: enhancing form

--- src/app/views/payment-link/payment-link.component.ts
+++ src/app/views/payment-link/payment-link.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective, FormSelectDirective } from '@coreui/angular';
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective, FormSelectDirective, InputGroupComponent, CalloutComponent } from '@coreui/angular';
 
 @Component({
   selector: 'app-payment-link',
   standalone: true,
-  imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, ReactiveFormsModule, FormsModule,FormSelectDirective, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective],
+  imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, ReactiveFormsModule, FormsModule,FormSelectDirective,InputGroupComponent,CalloutComponent, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective],
   templateUrl: './payment-link.component.html',
   styleUrl: './payment-link.component.scss'
 })
